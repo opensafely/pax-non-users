59608ed7f6b7118a1bfc988f20d973d58ae289a8
Linda Nab <linda.nab@thedatalab.org>
2023-09-27 20:30:26 +0100

    add file for sense checking data flow

--- analysis/seq_trials/descriptives/describe_size_trials.R
+++ analysis/seq_trials/descriptives/describe_size_trials.R
@@ -33,6 +33,7 @@ args <- commandArgs(trailingOnly=TRUE)
 if(length(args)==0){
   # use for interactive testing
   period <- "month"
+  period_colname <- paste0("period_", period)
 } else {
   
   option_list <- list(
@@ -45,6 +46,7 @@ if(length(args)==0){
   opt <- parse_args(opt_parser)
   
   period <- opt$period
+  period_colname <- paste0("period_", period)
 }
 study_dates <-
   jsonlite::read_json(path = here::here("lib", "design", "study-dates.json")) %>%
@@ -53,11 +55,41 @@ study_dates <-
 ################################################################################
 # 0.3 Import data
 ################################################################################
+data <- 
+  read_rds(here("output", "data", "data_processed_excl_contraindicated.rds")) %>%
+  mutate(period = .data[[period_colname]])
 file_name <- paste0("data_seq_trials_", period, "ly.feather")
 trials <- arrow::read_feather(here::here("output", "data", file_name))
 
 ################################################################################
-# 1.0 Number of unique patients included in each arm of trials
+# 1.0 Sense check size of trials based on data
+################################################################################
+sense_check_size_trials <-
+  data %>%
+  group_by(period) %>%
+  summarise(n_total = n(),
+            n_pax = sum(treatment_strategy_cat_prim == "Paxlovid"),
+            n_sotmol = sum(treatment_strategy_cat_prim %in% c("Sotrovimab", "Molnupiravir")),
+            n_notrt = sum(treatment_strategy_cat_prim == "Untreated"),
+            n_sotmol0 = sum(treatment_strategy_cat_prim %in% c("Sotrovimab", "Molnupiravir") & tb_postest_treat == 0),
+            n_sotmol1 = sum(treatment_strategy_cat_prim %in% c("Sotrovimab", "Molnupiravir") & tb_postest_treat == 1),
+            n_sotmol2 = sum(treatment_strategy_cat_prim %in% c("Sotrovimab", "Molnupiravir") & tb_postest_treat == 2),
+            n_sotmol3 = sum(treatment_strategy_cat_prim %in% c("Sotrovimab", "Molnupiravir") & tb_postest_treat == 3),
+            n_sotmol4 = sum(treatment_strategy_cat_prim %in% c("Sotrovimab", "Molnupiravir") & tb_postest_treat == 4),
+            n_event0 = sum(status_primary == "covid_hosp_death" & fu_primary == 0),
+            n_event1 = sum(status_primary == "covid_hosp_death" & fu_primary == 1),
+            n_event2 = sum(status_primary == "covid_hosp_death" & fu_primary == 2),
+            n_event3 = sum(status_primary == "covid_hosp_death" & fu_primary == 3),
+            n_event4 = sum(status_primary == "covid_hosp_death" & fu_primary == 4),
+            n_censor0 = sum(status_primary != "covid_hosp_death" & fu_primary == 0),
+            n_censor1 =  sum(status_primary!= "covid_hosp_death" & fu_primary == 1),
+            n_censor2 =  sum(status_primary != "covid_hosp_death" & fu_primary == 2),
+            n_censor3 = sum(status_primary != "covid_hosp_death" & fu_primary == 3),
+            n_censor4 = sum(status_primary != "covid_hosp_death" & fu_primary == 4),
+            )
+
+################################################################################
+# 2.0 Number of unique patients included in each arm of trials
 ################################################################################
 size_trials <-
   trials %>%
@@ -73,7 +105,7 @@ size_trials <-
   rename(treated_baseline = n_1, untreated_baseline = n_0)
 
 ################################################################################
-# 2.0 Number of patients in untrt arm initiating treatment
+# 3.0 Number of patients in untrt arm initiating treatment
 ################################################################################
 n_init_trt_untrt_given_trial_period <- function(trials, period_no, trial_no){
   n_init_trt <- function(trials, period_no, trial_no, column_name){
@@ -103,7 +135,7 @@ n_init_trt_in_untrt_arm <-
           .f = ~ n_init_trt_untrt_all_trials(trials, .x))
 
 ################################################################################
-# 3.0 Join two tables from step 1 and 2
+# 4.0 Join two tables from step 1 and 2
 ################################################################################
 data_flow <-
   size_trials %>%
@@ -119,8 +151,10 @@ data_flow_red <-
   )
 
 ################################################################################
-# 4.0 Save output
+# 5.0 Save output
 ################################################################################
+write_csv(sense_check_size_trials,
+          fs::path(output_dir, "sense_check_for_data_flow.csv"))
 file_name <- paste0("data_flow_seq_trials_", period, "ly.csv")
 file_name_red <- paste0("data_flow_seq_trials_", period, "ly_red.csv")
 write_csv(data_flow,
