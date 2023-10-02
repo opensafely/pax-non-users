502ac9c09f4e4df67ead151e199a5a66a1650192
Linda Nab <linda.nab@thedatalab.org>
2023-10-02 16:42:22 +0100

    print speed diagnostics

--- analysis/seq_trials/prepare_data.R
+++ analysis/seq_trials/prepare_data.R
@@ -17,20 +17,29 @@ library(here)
 library(purrr)
 library(optparse)
 library(arrow)
+library(tictoc)
+print("Sourcing functions")
+tic()
 source(here::here("analysis", "seq_trials", "functions", "simplify_data.R"))
 source(here::here("analysis", "seq_trials", "functions", "split_data.R"))
 source(here::here("analysis", "seq_trials", "functions", "add_trt_lags.R"))
 source(here::here("analysis", "seq_trials", "functions", "construct_trials.R"))
+toc()
 
 ################################################################################
 # 0.1 Create directories for output
 ################################################################################
+print("Create output directory")
+tic()
 output_dir <- here::here("output", "data")
 fs::dir_create(output_dir)
+toc()
 
 ################################################################################
 # 0.2 Import command-line arguments
 ################################################################################
+print("Import command-line arguments")
+tic()
 args <- commandArgs(trailingOnly=TRUE)
 if(length(args)==0){
   # use for interactive testing
@@ -53,22 +62,31 @@ if(length(args)==0){
 study_dates <-
   jsonlite::read_json(path = here::here("lib", "design", "study-dates.json")) %>%
   map(as.Date)
+toc()
 
 ################################################################################
 # 0.3 Import data
 ################################################################################
+print("Import data")
+tic()
 data <- 
   read_feather(here("output", "data", "data_processed_excl_contraindicated.feather")) %>%
   mutate(period = .data[[period_colname]])
+toc()
 
 ################################################################################
 # 0.4 Data manipulation
 ################################################################################
+print("Split data")
+tic()
 data_splitted <-
   data %>%
   simplify_data() %>%
   split_data() %>%
   add_trt_lags()
+toc()
+size_data_splitted <- object.size(data_splitted)
+format(size_data_splitted, units = "Mb", standard = "SI")
 # make dummy data better
 if(Sys.getenv("OPENSAFELY_BACKEND") %in% c("", "expectations")){
   data_splitted <-
@@ -79,6 +97,8 @@ if(Sys.getenv("OPENSAFELY_BACKEND") %in% c("", "expectations")){
 ################################################################################
 # 1.0 Construct trials (eg monthly, bimonthly and weekly, depending on input args)
 ################################################################################
+print("Construct trials")
+tic()
 cuts <- data %>% pull(period) %>% unique() %>% sort()
 trials <-
   map_dfr(
@@ -88,9 +108,15 @@ trials <-
         data_splitted %>% filter(period == .x),
         5)
     )
+toc()
+size_trials <- object.size(trials)
+format(size_trials, units = "Mb", standard = "SI")
 
 ################################################################################
 # 2.0 Save output
-################################################################################.
+################################################################################
+print("Save output")
+tic()
 file_name <- paste0("data_seq_trials_", period, "ly.feather")
 write_feather(trials, fs::path(output_dir, file_name))
+toc()
